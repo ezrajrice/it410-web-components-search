34aadfd51106212a4d845417b85309842bc45ee2
Ezra Rice <Ezra Rice>
2017-02-08 19:05:39 -0700

    final commit

--- search.js
+++ search.js
@@ -1,38 +1,34 @@
-(function(window, document, undefined) {
+(function(window, document) {
 	"use strict";
 
     const template = `
         <div>
-            <input type="text" id="search-input"></input>
-            <button id="search-button">Search</button>
+            <input type="text"></input>
+            <button>Search</button>
         </div>
     `;
 	class Search extends HTMLElement {
 		
 		// A getter/setter for pressed property.
-        get search_value() {
-            return shadowRoot.querySelector('input').hasAttribute('value');
-        }
+        get value() {
+            return this.shadowRoot.querySelector('input').value;
+        };
 
-        set search_value(value) {
-            if (value) {
-                shadowRoot.querySelector('input').setAttribute('value', value);
-            } else {
-                shadowRoot.querySelector('input').removeAttribute('value');
-            }
-        }
+        set value(value) {
+        	this.setAttribute('value', value);
+            this.shadowRoot.querySelector('input').value = value;
+        };
 
 		constructor() {
             super();
             let shadowRoot = this.attachShadow({mode: 'open'});
             shadowRoot.innerHTML = template;
 
-            shadowRoot.querySelector('input').addEventListener('keydown', 
+            shadowRoot.querySelector('input').addEventListener('keyup', 
             	function(e) {
             		if (!e) e = window.event;
-		        	// console.log(e.keyCode);
 					var enter_key = e.keyCode || e.which;
-					if (enter_key == "13") {
+					if (enter_key == '13') {
 						search();
 					}
             	}
@@ -44,10 +40,9 @@
         	);
             var search = function() {
 				console.log(shadowRoot.querySelector('input').value);
-			}
-        }
+        };
     };
 
-    window.customElements.define('search-pane', Search);
+    window.customElements.define('search-input', Search);
 
 })(window, document);
